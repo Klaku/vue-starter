43aad023e37e4498f6383962a3c56d2b17efa973
Rafał <rafass1000@gmail.com>
2022-03-08 15:09:50 +0100

    Status

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const Dialer = require("dialer").Dialer;
+let bridge;
 const config = {
   url: "https://uni-call.fcc-online.pl",
-  login: "<login>",
-  password: "<haslo>"
+  login: "focus01",
+  password: "234rwefgdfb",
 };
 Dialer.configure(config);
 app.listen(3000, () => {
   console.log("app listening on port 3000");
 });
-app.get("/call/:number1/:number2", (req, res) => {
+
+app.get("/call/:number1/:number2", async (req, res) => {
   const number1 = req.params.number1;
   const number2 = req.params.number2;
-  Dialer.call(number1, number2);
+  bridge = await Dialer.call(number1, number2);
   res.json({ success: true });
+});
+
+app.get("/status", async (req, rep) => {
+  let status = "NONE";
+  if (bridge !== null) {
+    status = await bridge.getStatus();
+  }
+  res.json({ success: true, status: status });
 });
